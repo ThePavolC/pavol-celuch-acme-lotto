767e3e3c5c158552549174628a6c97846639e262
ThePavolC <celuch.pavol@gmail.com>
2023-09-28 20:47:09 +0200

    Adding support for frontend. Adding a React app with webpack, babel, mobx, axios, bootstrap

--- core/project/settings.py
+++ core/project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "webpack_loader",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = "core.project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,7 +120,22 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+WEBPACK_LOADER = {
+    "DEFAULT": {
+        # location of Webpack generated files
+        "BUNDLE_DIR_NAME": "dist/",
+        # where to look for status of the Webpack process
+        "STATS_FILE": os.path.join(BASE_DIR, "webpack-stats.json"),
+        # recommended from the project README
+        "CACHE": not DEBUG,
+        "POLL_INTERVAL": 0.1,
+        "IGNORE": [r".+\.hot-update.js", r".+\.map"],
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require("path");
+var BundleTracker = require("webpack-bundle-tracker");
+
+module.exports = {
+    mode: "development",
+
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.join(__dirname, "assets/dist"),
+    },
+
+    entry: path.join(__dirname, "assets/src/js/index"),
+
+    output: {
+        path: path.join(__dirname, "assets/dist"),
+        filename: "[name]-[hash].js",
+        clean: true,
+    },
+
+    plugins: [
+        new BundleTracker({
+            path: __dirname,
+            filename: "webpack-stats.json",
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
