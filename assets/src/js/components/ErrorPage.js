29f52e4cc9bb04a065cc7a4cd583973b5155f5eb
ThePavolC <celuch.pavol@gmail.com>
2023-10-02 15:15:42 +0200

    Adding webpack image loader rule and improving error page

--- assets/src/js/components/ErrorPage.js
+++ assets/src/js/components/ErrorPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import image404 from "../images/404.webp";
+
 export default function ErrorPage() {
     return (
         <div id="error-page">
-            <h1>Oops!</h1>
+            <h1>Congrats!</h1>
             <p>You found a non existing page.</p>
+            <img src={image404} alt="404cat" />
         </div>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(png|jpg|webp|gif|svg)$/,
+                type: "asset/resource",
+            },
         ],
     },
 };
